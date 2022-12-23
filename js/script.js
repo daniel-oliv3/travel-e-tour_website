4b8993a9e67f33796442ca3e091ddc35568a5d48
Daniel Oliveira <danielsapup3@gmail.com>
2022-12-23 10:42:37 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -21,8 +21,24 @@ var swiper = new Swiper(".home-slider", {
     },
 });
 
-
-
+/* ======= btn about us - swiper slider ======= */
+var swiper = new Swiper(".reviews-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoHeight: true,
+    grabCursor: true,
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    },
+});
 
 
 
